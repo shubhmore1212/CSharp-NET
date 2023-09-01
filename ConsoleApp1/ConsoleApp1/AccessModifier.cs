522899dfdb93aec6a6d8be26769493757767b6b6
More <Shubham.More@Klingelnberg.com>
2023-09-01 18:10:39 +0530

    added : Classes to understand access modifiers

---
+++ ConsoleApp1/ConsoleApp1/AccessModifier.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class AccessModifier
+    {   
+        private void Test1()
+        {
+            Console.WriteLine("Private Method");
+        }
+
+        internal void Test2()
+        {
+            Console.WriteLine("Internal Method");
+        }
+
+        protected void Test3()
+        {
+            Console.WriteLine("Protected Method");
+        }
+
+        protected internal void Test4()
+        {
+            Console.WriteLine("Protected Internal Method");
+        }
+
+        public void Test5()
+        {
+            Console.WriteLine("Public Method");
+        }
+        static void Main()
+        {
+            AccessModifier aM =new AccessModifier();
+
+            aM.Test1();
+            aM.Test2();
+            aM.Test3();
+            aM.Test4();
+            aM.Test5();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Two.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Two:AccessModifier
+    {
+        static void Main()
+        {
+            Two t=new Two();
+            //Child cannot acccess private method of parent class
+            //
+        }
+    }
+}
