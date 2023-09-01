7b7cb905d97133ddb226c7e26efaa8e9a5574b61
More <Shubham.More@Klingelnberg.com>
2023-09-01 17:07:37 +0530

    added : applictaion printing even numbers

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Please enter a valid number");
+          PrintNumbers(int.Parse(Console.ReadLine()));  
+        }
+
+        static void PrintNumbers(int number)
+        {
+            int Start = 0;
+            string MyVar = "";
+
+            while (Start <= number)
+            {
+                Console.Write(Start + " ");
+                Start += 2;
+            }
+            Console.WriteLine();
+        }
+    }
+
+  
+}
